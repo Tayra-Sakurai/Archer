3507f4fa912b5c996d8e9ff17103f4675db04f9f
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-16 17:10:41 +0900

    Some changes.

--- Caesar/ViewModels/LargeCategoriesViewModel.cs
+++ Caesar/ViewModels/LargeCategoriesViewModel.cs
@@ -1,12 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2026 Tayra Sakurai <tayra_sakurai@icloud.com>
+using Caesar.Contexts;
+using Caesar.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Caesar.ViewModels
 {
-    public partial class LargeCategoriesViewModel
+    public partial class LargeCategoriesViewModel : ObservableObject
     {
+        private readonly IDbContextFactory<CaesarContext> _contextFactory;
+
+        public LargeCategoriesViewModel(IDbContextFactory<CaesarContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+            LargeCategories = [];
+        }
+
+        [ObservableProperty]
+        public partial ObservableCollection<LargeCategory> LargeCategories { get; set; }
     }
 }
